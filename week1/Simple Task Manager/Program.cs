18ea0c1dee9ac15bb18f8a4f534109d898a8eca9
deborah <deborahberhanu644@gmail.com>
2023-09-05 13:59:00 +0300

    new

---
+++ week1/Simple Task Manager/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+public enum TaskCategory {
+    Personal,
+    Work,
+    Errands,
+    Family
+}
+
+public class Task {
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public TaskCategory Category { get; set; }
+    public bool IsCompleted { get; set; }
+}
+
+public class TaskManager {
+    public List<Task> Tasks { get; set; }
+    public string filepath;
+    public TaskManager(string path) {
+        filepath = path;
+        Tasks = LoadTask().Result; 
+    }
+
+    private async Task<bool> SaveTasks(List<Task> tasks) {
+        List<string> lines = new List<string>();
+        foreach (var task in tasks) {
+            lines.Add(TaskToString(task));
+        }
+        await File.WriteAllLinesAsync(filepath, lines);
+        return true;
+    }
+
+    public async Task<List<Task>> LoadTask() {
+        var taskfromfile = await File.ReadAllLinesAsync(filepath);
+        List<Task> loaded = new List<Task>();
+        foreach (string line in taskfromfile) {
+            loaded.Add(filetotask(line));
+        }
+        return loaded;
+    }
+
+    public Task filetotask(string line) {
+        string[] taskproperties = line.Split("|");
+        Task task = new Task() {
+            Name = taskproperties[0],
+            Description = taskproperties[1],
+            Category = Enum.TryParse(taskproperties[2], out TaskCategory category)
+                        ? category
+                        : TaskCategory.Personal,
+            IsCompleted = bool.Parse(taskproperties[3])
+        };
+        return task;
+    }
+
+    public string TaskToString(Task task) {
+        return $"{task.Name}|{task.Description}|{task.Category}|{task.IsCompleted}";
+    }
+
+    public async Task<List<Task>> FilterTasks(TaskCategory category) {
+        var tasks = await LoadTask();
+        var filteredTasks = tasks.Where(task => task.Category == category).ToList();
+        return filteredTasks;
+    }
+
+    public async Task<int> AddTask(Task task) {
+        var tasks = await LoadTask();
+        tasks.Add(task);
+        await SaveTasks(tasks); 
+        return 0;
+    }
+
+    public async Task<int> UpdateTask(string taskName, string updatedName, string taskDescription, TaskCategory category, bool isCompleted) {
+        var tasks = await LoadTask();
+        foreach (var task in tasks) {
+            if (task.Name == taskName) {
+                task.Name = updatedName;
+                task.Description = taskDescription;
+                task.Category = category;
+                task.IsCompleted = isCompleted;
+                break;
+            }
+        }
+        await SaveTasks(tasks);
+        return 0;
+    }
+
+    public void DisplayTasks() {
+        foreach (var task in Tasks) {
+            Console.WriteLine($"Name: {task.Name}, Description: {task.Description}, Category: {task.Category}, Is Completed: {task.IsCompleted}");
+        }
+    }
+}
+
+class Program {
+    static async Task<int> Main(string[] args) {
+        TaskManager taskManager;
+        try {
+            taskManager = new TaskManager("lists.txt");
+            Console.Write("Enter the number of tasks to add: ");
+            int numberOfTasks = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfTasks; i++) {
+                Console.WriteLine($"Enter details for Task {i + 1}:");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Description: ");
+                string description = Console.ReadLine();
+                Console.Write("Category (Personal, Work, Errands, Family): ");
+                Enum.TryParse(Console.ReadLine(), out TaskCategory category);
+                Console.Write("Is Completed (True/False): ");
+                bool.TryParse(Console.ReadLine(), out bool isCompleted);
+
+                await taskManager.AddTask(new Task { Name = name, Description = description, Category = category, IsCompleted = isCompleted });
+            }
+            taskManager.DisplayTasks();
+           Console.Write("Do you want to update a task? (yes/no): ");
+            string confirm = Console.ReadLine();
+
+            if (confirm.ToLower() == "yes") {
+                Console.WriteLine("Enter the name of the task to update:");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Enter the updated task name:");
+                string upname = Console.ReadLine();
+
+                Console.WriteLine("Enter the updated task description:");
+                string udesc = Console.ReadLine();
+
+                Console.WriteLine("Enter the updated task Category (Personal, Work, Errands, Family):");
+                Enum.TryParse(Console.ReadLine(), out TaskCategory ucategory);
+
+                Console.WriteLine("Is the task completed? (true/false):");
+                bool.TryParse(Console.ReadLine(), out bool uiscompleted);
+
+                await taskManager.UpdateTask(name, upname, udesc, ucategory, uiscompleted);
+            } 
+            Console.WriteLine("Enter a category you want the task to be filterd(Personal, Work, Errands, Family)");
+            Enum.TryParse(Console.ReadLine(), out TaskCategory cat);
+            List<Task> filteredTasks = await taskManager.FilterTasks(cat);
+            foreach (var t in filteredTasks) {
+            Console.WriteLine($"Name: {t.Name}, Description: {t.Description}, Category: {t.Category}, Is Completed: {t.IsCompleted}");
+        }
+
+        } catch (Exception ex) {
+            Console.WriteLine(ex.Message);
+        }
+        return 0;
+    }
+}
---
+++ week1/Student Management System/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+public class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public readonly int RollNumber;
+    public string Grade { get; set; }
+
+    public Student(int rollNumber)
+    {
+        RollNumber = rollNumber;
+    }
+}
+
+class StudentList<T>
+{
+    public List<Student> Students;
+
+    public StudentList(List<Student> students)
+    {
+        Students = students;
+    }
+
+    public void Filter(int optionalRollNumber = 0, string optionalName = null)
+    {
+        var filteredStudents = Students.FindAll(stu => stu.RollNumber == optionalRollNumber || stu.Name == optionalName);
+        PrintQueryResults(filteredStudents);
+    }
+
+    public void PrintQueryResults(List<Student> students)
+    {
+        foreach (var student in students)
+        {
+            Console.WriteLine(student.RollNumber + ": " + student.Name);
+        }
+    }
+
+    public void SaveToJson(string filePath)
+    {
+        var json = JsonConvert.SerializeObject(this);
+        File.WriteAllText(filePath, json);
+    }
+
+    public static StudentList<T> LoadFromJson(string filePath)
+    {
+        var json = File.ReadAllText(filePath);
+        return JsonConvert.DeserializeObject<StudentList<T>>(json);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var students = new List<Student>
+        {
+            new Student(1) { Name = "Alice", Age = 20, Grade = "A" },
+            new Student(2) { Name = "Bob", Age = 21, Grade = "B" },
+            new Student(3) { Name = "Charlie", Age = 19, Grade = "C" }
+        };
+
+        var studentList = new StudentList<Student>(students);
+        studentList.Filter(optionalRollNumber: 3);
+
+        studentList.SaveToJson("studentList.json");
+
+        var loadedStudentList = StudentList<Student>.LoadFromJson("studentList.json");
+
+        Console.WriteLine("Deserialized objects are:");
+        foreach (var student in loadedStudentList.Students)
+        {
+            Console.WriteLine($"Roll Number: {student.RollNumber}");
+            Console.WriteLine($"Name: {student.Name}");
+            Console.WriteLine($"Age: {student.Age}");
+            Console.WriteLine($"Grade: {student.Grade}");
+            Console.WriteLine();
+        }
+    }
+}
+
